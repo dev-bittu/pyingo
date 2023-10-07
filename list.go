e2cfccd567e114d16acd021075c11f8f70a18781
DevBittu <devbittu@proton.me>
2023-10-07 19:39:55 +0530

    format code & improve repr

--- list.go
+++ list.go
@@ -1,35 +1,35 @@
 package pyingo
 
 type List struct {
-  Items []interface{}
+	Items []interface{}
 }
 
 func (l *List) String() string {
-  repr := ""
-  for _, i := range l.Items {
-    repr += i.(string)
-  }
-  return repr
+	repr := ""
+	for _, i := range l.Items {
+		repr += i.(string)
+	}
+	return "[" + repr + "]"
 }
 
 func (l *List) Append(element interface{}) {
-  l.Items = append(l.Items, element)
+	l.Items = append(l.Items, element)
 }
 
 func (l *List) Pop() interface{} {
-  if len(l.Items) == 0 {
-    panic("IndexError: list is empty")
-  }
-  index := len(l.Items)-1
-  element := l.Items[index]
-  l.Items = l.Items[:index]
-  return element
+	if len(l.Items) == 0 {
+		panic("IndexError: list is empty")
+	}
+	index := len(l.Items) - 1
+	element := l.Items[index]
+	l.Items = l.Items[:index]
+	return element
 }
 
 func (l *List) Clear() {
-  clear(l.Items)
+	clear(l.Items)
 }
 
 func (l *List) Extend(another_list List) {
-  l.Items = append(l.Items, another_list.Items...)
+	l.Items = append(l.Items, another_list.Items...)
 }
