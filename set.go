1497d242f06601bbb589aaa3441decfe90cb55ae
dev-bittu <devbittu@proton.me>
2023-10-11 05:54:22 +0530

    added add & union methods

--- set.go
+++ set.go
@@ -13,3 +13,22 @@ func (s *Set) String() string {
   }
   return "{"+repr+"}"
 }
+
+func (s *Set) Add(element interface{}) {
+  s.Items[element] = true
+}
+
+func (s *Set) Union(set2 Set) Set {
+  final_set := Set{}
+  for i, ok := range s.Items {
+    if ok {
+      final_set.Items[i] = true
+    }
+  }
+  for i, ok := range set2.Items {
+    if ok {
+      final_set.Items[i] = true
+    }
+  }
+  return final_set
+}
